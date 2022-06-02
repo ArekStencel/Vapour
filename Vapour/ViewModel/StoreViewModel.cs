960657ded8ad37bf32831ac6b1f97b80c24f5f1a
milordmariusz <komodo454@wp.pl>
2022-06-02 18:00:04 +0200

    Add listView and showing game data

--- Vapour/ViewModel/StoreViewModel.cs
+++ Vapour/ViewModel/StoreViewModel.cs
@@ -11,11 +11,10 @@ namespace Vapour.ViewModel
     public class StoreViewModel : BaseViewModel
     {
         private readonly VapourDatabaseEntities _dataContext;
-
         private readonly IAuthenticator _authenticator;
 
-        private List<string> _games = new List<string>();
-        public List<string> Games
+        private List<Game> _games = new List<Game>();
+        public List<Game> Games
         {
             get => _games;
             set
@@ -25,12 +24,103 @@ namespace Vapour.ViewModel
             }
         }
 
+        private Game _selectedGame;
+        public Game SelectedGame
+        {
+            get => _selectedGame;
+            set
+            {
+                _selectedGame = value;
+                Title = value.Title;
+                Price = value.Price.ToString();
+                Genre = value.Genre.Name;
+                Description = value.Description;
+                ReleaseDate = value.ReleaseDate.ToString();
+                AverageRate = GetAverageRate();
+
+                OnPropertyChanged(nameof(SelectedGame));
+            }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        private string _price;
+        public string Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        private string _genre;
+        public string Genre
+        {
+            get => _genre;
+            set
+            {
+                _genre = value;
+                OnPropertyChanged(nameof(Genre));
+            }
+        }
+
+        private string _releaseDate;
+        public string ReleaseDate
+        {
+            get => _releaseDate;
+            set
+            {
+                _releaseDate = value;
+                OnPropertyChanged(nameof(ReleaseDate));
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        private string _averageRate;
+        public string AverageRate
+        {
+            get => _averageRate;
+            set
+            {
+                _averageRate = value;
+                OnPropertyChanged(nameof(AverageRate));
+            }
+        }
+
+        private string GetAverageRate()
+        {
+
+            return 0.ToString();
+        }
+
         private void GetAllGames()
         {
             var games = _dataContext.Games.ToList();
-            foreach (var game in games.Where(user => _games != null))
+
+            foreach (var game in games)
             {
-                _games.Add(game.ToString());
+               _games.Add(game);
             }
         }
 
@@ -39,8 +129,6 @@ namespace Vapour.ViewModel
             _dataContext = dataContext;
             _authenticator = authenticator; 
             GetAllGames();
-
-
         }
     }
 }
