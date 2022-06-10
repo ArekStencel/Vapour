72a46a075d28ca0fca8d97f4acd76092af0c9032
Patryk <patrykyrtap@o2.pl>
2022-06-10 17:58:37 +0200

    Create userservice

---
+++ Vapour/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Vapour.Services
+{
+    public interface IUserService
+    {
+        Task<bool> UpdateWalletBalance(int userId, decimal value);
+    }
+}
